1a7c94096c14035d4ddc66153f9e4c1539f2347a
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-03-26 19:19:08 -0300

    Exportado arquivo de configurações do gráfico para fora da página index

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE>
-<!-- This code is for demonstration purposes only.  You should not hotlink to Github, Rawgit, or files from the Cytoscape.js documentation in your production apps. -->
 <html>
 <head>
-    <title>cytoscape-dagre.js demo</title>
+    <title>Make RPG Stories</title>
     <!-- CYTOSCAPE -->
-    <!--  -->
+    <!-- START -->
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
     <link href="css/style.css" rel="stylesheet" />
     <script src="js/cytoscape.min.js"></script>
     <script src="js/dagre.min.js"></script>
     <script src="js/cytoscape-dagre.js"></script>
+    <!-- END -->
     <!-- CYTOSCAPE -->
-    <!--  -->
     <!-- JQUERY -->
     <script src="js/JQuery.js"></script>
     <!-- MATERIALIZE CSS-->
@@ -28,64 +27,12 @@
 <body>
     <div class="row">
         <div class="col l4" style="border: 1px solid black">
-            TESTE
             <div id="cy"></div>
         </div>    
     </div>
-    <!-- Load application code at the end to ensure DOM is loaded -->
-    <!-- <script src="js/code.php"></script> -->
-    <script type="text/javascript">
-
-        $( document ).ready(function() {
-            var cy = window.cy = cytoscape({
-                container: document.getElementById('cy'),
-
-                boxSelectionEnabled: false,
-                autounselectify: true,
-
-                layout: {
-                    name: 'dagre'
-                },
-
-                style: [
-                {
-                  selector: 'node',
-                  style: {
-                      'content': 'data(id)',
-                      'text-opacity': 0.5,
-                      'text-valign': 'center',
-                      'text-halign': 'right',
-                      'background-color': '#11479e',
-                      'width': 10,
-                      'height': 10
-                  }
-              },
-
-              {
-                  selector: 'edge',
-                  style: {
-                      'curve-style': 'bezier',
-                      'width': 1.5,
-                      'target-arrow-shape': 'triangle',
-                      'line-color': '#9dbaea',
-                      'target-arrow-color': '#9dbaea',
-                  }
-              }
-              ],
-
-              elements: {
-                nodes: [
-                <?php include("js/data.php") ?>
-                ],
-                edges: [
-
-                ]
-            },
-        });
-            cy.zoomingEnabled(false);
-        });
-
-    </script>
-
+    <!-- CARREGA O GRÁFICO -->
+    <?php
+    	include("graph.php");
+    ?>
 </body>
 </html>
