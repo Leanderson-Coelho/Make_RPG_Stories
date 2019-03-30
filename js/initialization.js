d273025b1010a4cd05b60bcd50076bda4c37e3f9
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-03-29 22:57:12 -0300

    Adicionando inicialização dos collapsibel.

--- js/initialization.js
+++ js/initialization.js
@@ -2,3 +2,13 @@
 $(document).ready(function(){
 	$('select').formSelect();
 });
+
+// INICIALIZA INPUTS
+$(document).ready(function() {
+	$('input#input_text, textarea#textarea2').characterCounter();
+});
+
+// INICIA COLLAPSIBLE
+$(document).ready(function(){
+	$('.collapsible').collapsible();
+});
